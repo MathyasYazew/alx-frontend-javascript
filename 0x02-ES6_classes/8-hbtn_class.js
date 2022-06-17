0c911d5bd97e2fbd37d2a3160a5a2f5109e8e2a7
MathyasYazew <88359082+MathyasYazew@users.noreply.github.com>
2022-06-17 13:41:24 +0300

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -3,10 +3,3 @@ export default class HolbertonClass {
     this._size = size;
     this._location = location;
   }
-
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'number') return this._size;
-    if (hint === 'string') return this._location;
-    return this;
-  }
-}
