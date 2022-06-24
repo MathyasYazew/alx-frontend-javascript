4d47a5b293f74c76470f1e4d7211600e282c60a2
MathyasYazew <88359082+MathyasYazew@users.noreply.github.com>
2022-06-24 19:23:39 +0300

    Delete 0x04-TypeScript directory

--- 0x04-TypeScript/task_3/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var CRUD = require("./crud");
-var obj = {
-    firstName: "Guillaume",
-    lastName: "Salva"
-};
-var newRowID = CRUD.insertRow(obj);
-console.log(newRowID);
